be429a811cdb7e08f720328696615a66f463f04d
shleger <sh2a@yandex.ru>
2013-07-31 21:45:12 +0400

    add test stub

---
+++ spring-reqfact/src/test/java/ru/saa/part/springrf/server/service/EmployeeServiceImplTest.java
@@ -0,0 +1,38 @@
+package ru.saa.part.springrf.server.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: saa
+ * Date: 7/31/13
+ * Time: 21:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath*:applicationContext-test.xml")
+public class EmployeeServiceImplTest {
+
+//    @Autowired
+//    EmployeeServiceImpl employeeService;
+
+    @Test
+    public void testFindEmployee() throws Exception {
+
+    }
+
+    @Test
+    public void testPersist() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEmployees() throws Exception {
+
+    }
+}
