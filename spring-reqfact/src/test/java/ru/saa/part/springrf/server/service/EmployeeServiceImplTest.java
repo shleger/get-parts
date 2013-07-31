2b2a9e1670a877201dc9b85c4ac8895ec7d0cd6b
shleger <sh2a@yandex.ru>
2013-07-31 23:11:41 +0400

    same err

--- spring-reqfact/src/test/java/ru/saa/part/springrf/server/service/EmployeeServiceImplTest.java
+++ spring-reqfact/src/test/java/ru/saa/part/springrf/server/service/EmployeeServiceImplTest.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.saa.part.springrf.server.domain.Employee;
+import ru.saa.part.springrf.server.domain.Job;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -31,9 +36,24 @@ public class EmployeeServiceImplTest {
     @Test
     public void testPersist() throws Exception {
 
+        System.out.println("-----------testPersist---------------");
+
+        Random random = new Random();
+        final int RND_MAX = 99;
+
+
         Employee emp = new Employee();
         emp.setDepartment("testDep");
         emp.setDisplayName("testDispName");
+
+        List<Job> jobs = new ArrayList<Job>();
+        Job job =  new Job();
+        job.setFirstName("first Job" + random.nextInt(RND_MAX));
+        job.setLastName("last Job" + random.nextInt(RND_MAX));
+        jobs.add(job);
+
+        emp.setJobs(jobs); ///TODO same err:   javax.persistence.PersistenceException: org.hibernate.exception.ConstraintViolationException: NULL not allowed for column "EMPLOYEE_ID"; SQL statement:
+
 
 
         employeeService.persist(emp);
